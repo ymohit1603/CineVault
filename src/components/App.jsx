4722e8964a1a63d8ad0dcd2c19a562cecb237144
ymohit1603 <mohityadav0330@gmail.com>
2024-10-25 09:03:01 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CssBaseLine } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <CssBaseLine />
+      <div>App</div>
+    </div>
+  );
+}
+
+export default App;
