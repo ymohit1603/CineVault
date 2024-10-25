eacf619874005ffedc4b5da76a0749587a1f9538
ymohit1603 <mohityadav0330@gmail.com>
2024-10-25 20:32:33 +0530

    add: redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { CssBaseLine } from '@mui/material';
+import React, { useRef } from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import useStyles from './styles';
+// import useAlan from './Alan';
+
+import {
+  Movies, Actors, MovieInfo, Navbar, Profile,
+} from './index';
 
 function App() {
+  const classes = useStyles();
+  const alanBtnContainer = useRef();
+
+  // useAlan();
+
   return (
-    <div>
-      <CssBaseLine />
-      <div>App</div>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/approved" element={<Movies />} />
+          <Route exact path="/movie/:id" element={<MovieInfo />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+      <div ref={alanBtnContainer} />
     </div>
   );
 }
